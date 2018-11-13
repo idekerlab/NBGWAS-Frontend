840a373365c3717f8c002f44f12be28872a55949
Brett Settle <brettjsettle@gmail.com>
2018-11-13 13:30:18 -0800

    Updates

---
+++ precache-manifest.4c75c7771903cf9d34d6368276a44a54.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b67033135f15d9729c77",
+    "url": "/NBGWAS-Frontend/static/js/runtime~main.b6703313.js"
+  },
+  {
+    "revision": "5045b77e173db255e065",
+    "url": "/NBGWAS-Frontend/static/js/main.5045b77e.chunk.js"
+  },
+  {
+    "revision": "ee1d644e9f03269c8166",
+    "url": "/NBGWAS-Frontend/static/js/1.ee1d644e.chunk.js"
+  },
+  {
+    "revision": "5045b77e173db255e065",
+    "url": "/NBGWAS-Frontend/static/css/main.1458d53f.chunk.css"
+  },
+  {
+    "revision": "5ac8cc43f1c46d932da35f1104976dfe",
+    "url": "/NBGWAS-Frontend/index.html"
+  }
+];
