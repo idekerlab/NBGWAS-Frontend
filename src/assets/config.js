bc6b6fb623b133b742ff35ca60c0df38a8d4fc00
Tongqiu Jia <t1jia@ucsd.edu>
2019-07-01 10:53:13 -0700

    version 0.1.6

--- src/assets/config.js
+++ src/assets/config.js
@@ -4,7 +4,7 @@ const data = {
     title: 'Network Assisted Genomic Analysis',
     subheader: 'Network Assisted Genomic Analysis re-prioritizes significant single nucleotide polymorphisms ' +
         '(SNPs) to genes using network diffusion methods including random walk and heat diffusion.',
-    version: '0.1.5',
+    version: '0.1.6',
     url: {
         sample_file: '/nagadata/schizophrenia.txt',
         sample_results: '/nagadata/example_output/example_schizophrenia_output.json',
